9611654ac095cbea6ac1ffa09b0c3c67026c9869
Alessandro Liuzzi <61493515+alessandroliuzzi@users.noreply.github.com>
2025-06-14 17:30:08 +0200

    Update resnet3D18_finetuned.py
    Added normalization to input data according to Kinetics-400, used for the pretraining of the Resnet3D model

--- resnet3D18_finetuned.py
+++ resnet3D18_finetuned.py
@@ -54,7 +54,9 @@ class VideoDataset(Dataset):
 # === Transform ===
 transform = transforms.Compose([
     transforms.Resize((112, 112)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
 ])
 
 # === Load DataFrame and sample with stratification ===
